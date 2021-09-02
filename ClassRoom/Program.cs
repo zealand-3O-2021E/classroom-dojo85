a726239298dbc9ac6cba331575c741518f46833b
Niko D <nikodojan@live.de>
2021-09-02 09:26:14 +0200

    data added

--- ClassRoom/Program.cs
+++ ClassRoom/Program.cs
@@ -10,6 +10,15 @@ namespace ClassRoom
     {
         static void Main(string[] args)
         {
+            ClassRoom classRoom = new ClassRoom()
+            {
+                ClassName = "Q3",
+                SemesterStart = new DateTime(2019, 08, 26)
+            };
+
+            classRoom.ClassList.Add(new Student("Niko", 12, 21));
+            classRoom.ClassList.Add(new Student("Adriana", 09, 08));
+            classRoom.ClassList.Add(new Student("Nohely", 07, 14));
         }
     }
 }
