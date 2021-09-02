393b56e446eebce6d190778ec6134e9efd711528
Niko D <nikodojan@live.de>
2021-09-02 10:04:55 +0200

    Task 6, solution without link, functions

--- ClassRoom/Program.cs
+++ ClassRoom/Program.cs
@@ -29,7 +29,43 @@ namespace ClassRoom
             {
                 Console.WriteLine($"Student Name: {student.Name}, Birthday: {student.BirthDay}.{student.BirthMonth}.");
             }
+
+            CountBirthsPerSeason(classRoom.ClassList);
             
+
+        }
+
+        // Task 5
+        private static string Season(int month)
+        {
+            if (month > 2 && month < 6) return "Spring";
+            if (month > 5 && month < 9) return "Summer";
+            if (month > 8 && month < 12) return "Autumn";
+            return "Winter";
+        }
+
+        // Task 6
+        private static void CountBirthsPerSeason(List<Student> students)
+        {
+            Dictionary<string, int> birthSeasons = new Dictionary<string, int>(4);
+
+            foreach (var student in students)
+            {
+                string season = Season(student.BirthMonth);
+                if (!birthSeasons.Keys.Contains(season))
+                {
+                    birthSeasons.Add(season, 1);
+                }
+                else
+                {
+                    birthSeasons[season] += 1;
+                }
+            }
+
+            foreach (var season in birthSeasons)
+            {
+                Console.WriteLine(season.Key + ", " + season.Value);
+            }
         }
     }
 }
