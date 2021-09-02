9b7edc17762cde153772ef2bd5073250a502fa7b
Niko D <nikodojan@live.de>
2021-09-02 09:15:50 +0200

    Student class created

---
+++ ClassRoom/Student.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassRoom
+{
+    public class Student
+    {
+        public string Name { get; private set; }
+        public int BirthMonth { get; private set; }
+        public int BirthDay { get; private set; }
+
+        public Student(string name, int birthMonth, int birthDay)
+        {
+            Name = name;
+            BirthMonth = birthMonth;
+            BirthDay = birthDay;
+        }
+    }
+}
