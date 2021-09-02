821a613d2eb9f39d0ede04cb71eead60a2181b75
Niko D <nikodojan@live.de>
2021-09-02 10:24:28 +0200

    Task 6 with LINQ

--- ClassRoom/Program.cs
+++ ClassRoom/Program.cs
@@ -21,18 +21,39 @@ namespace ClassRoom
             classRoom.ClassList.Add(new Student("Niko", 12, 21));
             classRoom.ClassList.Add(new Student("Adriana", 09, 08));
             classRoom.ClassList.Add(new Student("Nohely", 07, 14));
+            classRoom.ClassList.Add(new Student("Vladimir", 03, 10));
+            classRoom.ClassList.Add(new Student("Nathan", 08, 20));
 
             // Task 4
-            Console.WriteLine($"Class name: {classRoom.ClassName}, Semester beginn: {classRoom.SemesterStart}");
+            Console.WriteLine($"Class name: {classRoom.ClassName}, " +
+                              $"Semester beginn: {classRoom.SemesterStart.ToShortDateString()} \n");
             Console.WriteLine("Enrolled students:");
             foreach (var student in classRoom.ClassList)
             {
-                Console.WriteLine($"Student Name: {student.Name}, Birthday: {student.BirthDay}.{student.BirthMonth}.");
+                Console.WriteLine($"Student Name: {student.Name} \n\t Birthday: {student.BirthDay}.{student.BirthMonth}.");
             }
 
-            CountBirthsPerSeason(classRoom.ClassList);
+            Console.WriteLine();
+            BirthsPerSeason(classRoom.ClassList);
             
+            Console.WriteLine("\nPress any key to close console...");
+            Console.ReadKey();
+        }
 
+        // Task 6
+        private static void BirthsPerSeason(List<Student> students)
+        {
+            var result = students.GroupBy(s => Season(s.BirthMonth)).Select(g => new
+            {
+                Season = g.Key,
+                Amount = g.Count()
+            });
+
+            Console.WriteLine("Birthdays in each season:");
+            foreach (var season in result)
+            {
+                Console.WriteLine($"{season.Season}: {season.Amount} birthdays");
+            }
         }
 
         // Task 5
@@ -42,30 +63,6 @@ namespace ClassRoom
             if (month > 5 && month < 9) return "Summer";
             if (month > 8 && month < 12) return "Autumn";
             return "Winter";
-        }
-
-        // Task 6
-        private static void CountBirthsPerSeason(List<Student> students)
-        {
-            Dictionary<string, int> birthSeasons = new Dictionary<string, int>(4);
-
-            foreach (var student in students)
-            {
-                string season = Season(student.BirthMonth);
-                if (!birthSeasons.Keys.Contains(season))
-                {
-                    birthSeasons.Add(season, 1);
-                }
-                else
-                {
-                    birthSeasons[season] += 1;
-                }
-            }
-
-            foreach (var season in birthSeasons)
-            {
-                Console.WriteLine(season.Key + ", " + season.Value);
-            }
         }
     }
 }
